ef26e81fdb193ff3ed3dcd3fb23778d1fb075496
jorgealferez <106700755+jorgealferez@users.noreply.github.com>
2025-02-20 01:15:39 +0100

    [TASK-23] : Crear Layout Corporativo Moderno en Laravel 10.x

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Rutas Web
+|--------------------------------------------------------------------------
+|
+| Aquí es donde se pueden registrar las rutas web para tu aplicación.
+| Estas rutas son cargadas por el RouteServiceProvider dentro de un grupo que
+| contiene el grupo de middleware "web". ¡Disfruta construyendo tu aplicación!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
